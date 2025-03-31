e9aba34ce844dff182c6ab713f220b9123868c62
Sai Asritha Kunduru <asritha1813@gmail.com>
2025-03-30 23:24:16 -0400

    open-graph prorocols and components

---
+++ my-app/src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <h1>Welcome to Little Lemon</h1>
+      <p>Reserve your table online quickly and easily.</p>
+    </main>
+  );
+}
+
+export default Main;
---
+++ my-app/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#reserve">Reserve a Table</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
